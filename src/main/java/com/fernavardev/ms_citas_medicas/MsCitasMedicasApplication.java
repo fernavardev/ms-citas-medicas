90feefcb808ee2d80ca9358451c04ff56d6ffa96
Fernando <fer.navarrog@duocuc.cl>
2026-03-28 01:36:30 -0300

    inicializacion de proyecto y configuracion estructura base

---
+++ src/main/java/com/fernavardev/ms_citas_medicas/MsCitasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.fernavardev.ms_citas_medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCitasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCitasMedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernavardev/ms_citas_medicas/MsCitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernavardev.ms_citas_medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCitasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
